2ba29aab4735864622aabada05ae7ca4ef698abc
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-06-02 02:33:51 +0000

    same assignment 3.7

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,54 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static int searchMazeMoves(String[][] arr) {
+		
+		int col = 0;
+		int row = arr.length - 1;
+		int moves = 0;
+
+		
+
+		return mazeHelper(arr, col, row, moves);
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+	public static int mazeHelper(String[][] arr, int col, int row, int moves) {
+		
+		if (row < 0 || col < 0 || row >= arr.length || col >= arr[0].length || arr[row][col].equals("*")) {
+			return -1;
+		}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+		if (arr[row][col].equals("F")) {
+			return moves;
+		}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+		String temp = arr[row][col];
+		arr[row][col] = "*";
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+		int minMoves = -1;
+		
+		int up = mazeHelper(arr, col, row - 1, moves + 1);
+		if (up != -1 && (minMoves == -1 || up < minMoves)) {
+			minMoves = up;
+		}
 
+		int down = mazeHelper(arr, col, row + 1, moves + 1);
+		if (down != -1 && (minMoves == -1 || down < minMoves)) {
+			minMoves = down;
+		}
+
+		int left = mazeHelper(arr, col - 1, row, moves + 1);
+		if (left != -1 && (minMoves ==  -1 || left < minMoves)) {
+			minMoves = left;
+			
+		}
+
+		int right = mazeHelper(arr, col + 1, row, moves + 1);
+		if (right != -1 && (minMoves == -1 || right < minMoves)) {
+			minMoves = right;
+		}
+		
+		arr[row][col] = temp;
+		return minMoves;
+	} 
 }
